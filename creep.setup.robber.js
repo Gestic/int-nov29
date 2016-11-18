0b3a2f23684a9f8044efb858b636edb68a9a2236
AVATAR-Phoenix <bcarelse69@gmail.com>
2016-11-18 20:41:56 +0200

    Add files via upload
    robber setup file.

--- creep.setup.robber.js
+++ creep.setup.robber.js
@@ -2,22 +2,20 @@ var setup = new Creep.Setup('robber');
 setup.minControllerLevel = 3;
 setup.globalMeasurement = true;
 setup.measureByHome = true;
-setup.maxWeight = function(room){
-    return setup.maxCount(room) * 2000; 
-};
 setup.default = {
-    fixedBody: [CARRY, CARRY, MOVE], 
+    fixedBody: [], 
     multiBody: [CARRY, CARRY, MOVE], 
     minAbsEnergyAvailable: 150, 
     minEnergyAvailable: 0.4,
-	maxMulti: 10,
-	minMulti: (room) => (room.controller.level),
-    // maxCount: setup.maxCount, 
-    maxWeight: setup.maxWeight
+    //maxMulti: setup.maxMulti,
+    maxMulti: 10,
+    maxCount: 3, 
+    maxWeight: 2000,
+    minMulti: (room) => (room.controller.level)
 };
 setup.RCL = {
     1: setup.none,
-    2: setup.none,
+    2: setup.default,
     3: setup.default,
     4: setup.default,
     5: setup.default,
@@ -26,28 +24,3 @@ setup.RCL = {
     8: setup.default
 };
 module.exports = setup;
-
-// var setup = new Creep.Setup('privateer');
-// setup.minControllerLevel = 3;
-// setup.globalMeasurement = true;
-// setup.measureByHome = true;
-// setup.default = {
-    // fixedBody: [WORK, CARRY, MOVE], 
-    // multiBody: [WORK, CARRY, MOVE], 
-    // minAbsEnergyAvailable: 400, 
-    // minEnergyAvailable: 0.8,
-    // maxMulti: 10,
-    // minMulti: (room) => (room.controller.level),
-    // maxWeight: (room) => room.privateerMaxWeight
-// };
-// setup.RCL = {
-    // 1: setup.none,
-    // 2: setup.none,
-    // 3: setup.default,
-    // 4: setup.default,
-    // 5: setup.default,
-    // 6: setup.default,
-    // 7: setup.default,
-    // 8: setup.default
-// };
-// module.exports = setup;
