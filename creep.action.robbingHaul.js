90375cd3deae9976e587e9f6e5e35fdb49e25b7f
AVATAR-Phoenix <bcarelse69@gmail.com>
2016-11-20 16:33:57 +0200

    Its own action

---
+++ creep.action.robbingHaul.js
@@ -0,0 +1,39 @@
+var action = new Creep.Action('robbingHaul');
+action.maxPerTarget = 2;
+action.maxPerAction = 10;
+action.isValidAction = function(creep){
+    return ( creep.sum < creep.carryCapacity && (FlagDir.find(FLAG_COLOR.invade.robbing, creep.pos, true) != null) );
+};
+action.isValidTarget = function(target){
+    return ( target.store && _.sum(target.store) > 20 ) || ( target.energy && target.energy > 20 );
+};  
+action.newTarget = function(creep){
+    let that = this;
+    let target = creep.pos.findClosestByRange(creep.room.structures.all, {
+        filter: (structure) => that.isValidTarget(structure)
+    });
+    return target;
+};
+action.work = function(creep){
+    let ret = OK;
+    // has rampart? dismantle
+    let ramparts = _.filter(creep.room.lookForAt(LOOK_STRUCTURES, creep.target.pos), {'structureType': STRUCTURE_RAMPART });
+    let avoid = _.filter(creep.room.lookForAt(LOOK_STRUCTURES, creep.target.pos));
+    console.log(avoid);
+    if( ramparts.length > 0 ){
+        creep.move(TOP_RIGHT);
+        ret = creep.newTarget;
+    } else if( creep.target.store ) {
+        for( var type in creep.target.store ){
+            if( creep.target.store[type] > 0  )
+                ret = creep.withdraw(creep.target, type);
+        }
+    } else if ( creep.target.energy ) {
+        ret = creep.withdraw(creep.target, 'energy');
+    }
+    return ret;
+};
+action.onAssignment = function(creep, target) {
+    if( SAY_ASSIGNMENT ) creep.say(String.fromCharCode(9760), SAY_PUBLIC); 
+};
+module.exports = action;
